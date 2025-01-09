986e723fa649e636ee841cd005107fbde114405d
Aziz <mohamed-aziz.ben-ayed@epitech.eu>
2025-01-09 23:21:47 +0100

    fixing api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
     '740819517443-1h8rij2g7rlgof9gsvlrfovhnet7jtdm.apps.googleusercontent.com',
   GOOGLE_CLIENT_SECRET: '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',
   API_URL:
-    'https://p0bebn2b2g.execute-api.eu-west-3.amazonaws.com/dev/mypubli-cloud',
+    'https://p0bebn2b2g.execute-api.eu-west-3.amazonaws.com/dev/mypubli-cloud/',
 };
