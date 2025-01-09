2eb00d749c408ad2682378d98d7a57aa5e3e6b3b
Aziz <mohamed-aziz.ben-ayed@epitech.eu>
2025-01-09 23:18:05 +0100

    changing api url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,5 @@ export const environment = {
   GOOGLE_CLIENT_ID:
     '740819517443-1h8rij2g7rlgof9gsvlrfovhnet7jtdm.apps.googleusercontent.com',
   GOOGLE_CLIENT_SECRET: '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',
-  API_URL: 'http://localhost:3000/',
+  API_URL: 'http://localhost:3000/mypubli-cloud/',
 };
