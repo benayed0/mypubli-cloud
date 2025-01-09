ecbe120a8d067a90657f7c1f7c51cece01286770
Aziz <mohamed-aziz.ben-ayed@epitech.eu>
2025-01-09 21:47:55 +0100

    update API_URL to use HTTPS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
     '740819517443-1h8rij2g7rlgof9gsvlrfovhnet7jtdm.apps.googleusercontent.com',
   GOOGLE_CLIENT_SECRET: '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',
   API_URL:
-    'http://mypublic-cloud-api-env.eba-pbyem4jr.eu-central-1.elasticbeanstalk.com/',
+    'https://mypublic-cloud-api-env.eba-pbyem4jr.eu-central-1.elasticbeanstalk.com/',
 };
